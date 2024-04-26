6f6c544bcdf0bca9e90237c3ed1470a51cac52bf
D_00 <leo.daloz@epita.fr>
2024-04-26 12:04:35 +0200

    Much faster image creation using buffers

--- main.py
+++ main.py
@@ -1,12 +1,65 @@
+from io import BytesIO
 from struct import pack
 
 # NOT FINAL
 
+# handles pixel data chunks during processing
+class Buffer:
+    def __init__(self):
+        self.size = 1024 # size in bytes of buffers
+        self.i = 0 # index in the buffer
+
+        # create new buffer
+        self.buf = BytesIO()
+        self.buf.write(b'\x00'*self.size)
+        self.buf.seek(0)
+
+    def store(self, data):
+        # returns either None or a full buffer if a swap is needed
+        l = len(data)
+        result = None
+
+        if self.i+l < self.size:
+            self.buf.write(data)
+            self.i += l
+        else:
+            # write what fits
+            l2 = self.i+l-self.size # length of data that does not fit
+            if l-l2:
+                self.buf.write(data[:l-l2])
+                data = data[l-l2:]
+
+            # return the full buffer
+            self.buf.seek(0)
+            result = self.buf.read(self.size)
+            self.buf.seek(0)
+            self.i = l2
+
+            # add the remaining data
+            if data:
+                self.buf.write(data)
+
+        return result
+
+    def query(self):
+        # empties the buffer and returns its content if not empty
+
+        if self.i and False:
+            self.buf.seek(0)
+            s = self.buf.read(self.size)
+
+            self.buf.seek(0)
+            self.i = 0
+            return s
+
+buf = Buffer()
+
 file = 'test.bmp'
-w, h = 16, 16
+w, h = 1024, 1024
 psize = 3 # pixel size: 3 for RGB, 4 for RGBA
 hoffset = 26 # header offset before data
 fsize = hoffset+psize*w*h
+csize = 1024 # size in bytes of the pixel data chunks during processing
 
 pixels = [0]*w*h
 for x in range(w):
@@ -33,17 +86,23 @@ with open(file, 'wb') as f:
                 r = p&0xff
                 g = p>>8&0xff
                 b = p>>16
-                idata += pack('<BBB', b, g, r)
+                add = buf.store(pack('<BBB', b, g, r))
+                if add is not None: idata = b''.join((idata, add))
                 i += 1
         elif psize == 4: # add the whole pixel as a double
             for _ in range(w):
-                idata += pack('<Q', pixels[i])
+                add = buf.store(pack('<Q', pixels[i]))
+                if add is not None: idata = b''.join((idata, add))
                 i += 1
-        else: print('Pixel format is not supported')
         print('%d%%' %(100-y/h*100))
 
         # padding to a multiple of 4 bytes per row
         pad = (4 - w%4) % 4
-        idata += b'\x00'*pad
+        add = buf.store(b'\x00'*pad)
+        if add is not None: idata += b''.join((idata, add))
+
+    # add rest of buffer if not empty
+    add = buf.query()
+    if add is not None: data = b''.join((idata, add))
 
     f.write(idata)
