fd29e7ea6b10f684a55144ca0d442f28a78fee85
D_00 <leo.daloz@epita.fr>
2024-04-26 11:26:54 +0200

    Added temporary bmp generator

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 C/C++/Python library for processing really large images
 
 Python library is a work in progress, C/C++ version coming soon
+
+
+## TODO
+- custom color table for larger images, or save in 64K*64K chunks
---
+++ main.py
@@ -0,0 +1,49 @@
+from struct import pack
+
+# NOT FINAL
+
+file = 'test.bmp'
+w, h = 16, 16
+psize = 3 # pixel size: 3 for RGB, 4 for RGBA
+hoffset = 26 # header offset before data
+fsize = hoffset+psize*w*h
+
+pixels = [0]*w*h
+for x in range(w):
+    for y in range(h):
+        pixels[y*w + x] = (int(x*255/w) << 16) + int(y*255/h)
+
+BM = 19778 # bitmap signature
+pformat = '<' + 'B'*psize
+
+with open(file, 'wb') as f:
+    # BITMAPFILEHEADER
+    f.write(pack('<HLHHL', BM, fsize, 0, 0, hoffset))
+
+    # BITMAPCOREHEADER
+    f.write(pack('<LHHHH', 12, w, h, 1, psize<<3))
+
+    # Image Data
+    idata = b''
+    i = 0
+    for y in range(h-1, -1, -1): # reversed y order
+        if psize == 3: # separate pixel components and add them separately
+            for _ in range(w):
+                p = pixels[i]
+                r = p&0xff
+                g = p>>8&0xff
+                b = p>>16
+                idata += pack('<BBB', b, g, r)
+                i += 1
+        elif psize == 4: # add the whole pixel as a double
+            for _ in range(w):
+                idata += pack('<Q', pixels[i])
+                i += 1
+        else: print('Pixel format is not supported')
+        print('%d%%' %(100-y/h*100))
+
+        # padding to a multiple of 4 bytes per row
+        pad = (4 - w%4) % 4
+        idata += b'\x00'*pad
+
+    f.write(idata)
